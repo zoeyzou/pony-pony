c680ed965eaeeff34c1ef029e75ac42c291e3941
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-14 13:34:09 +0200

    Change how select pass up value

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,22 +1,49 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 
 import * as s from './select.styles'
 import { SelectOption } from './utils/types'
 import { Label } from 'components/label'
 
 interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  defaultValue?: string
   options: SelectOption<string>[]
+  onChangeHandler?: (val: string) => void
 }
 
-export const Select: FC<Props> = ({ onChange, options, title, ...rest }) => (
-  <s.SelectWrapper>
-    {title && <Label>{title}</Label>}
-    <s.Select {...rest} onChange={onChange}>
-      {options.map((option, index) => (
-        <s.Option key={index} value={option.value}>
-          {option.label}
+export const Select: FC<Props> = ({
+  defaultValue,
+  options,
+  title,
+  placeholder,
+  onChangeHandler,
+  ...rest
+}) => {
+  const [val, setVal] = useState<string>(defaultValue || '')
+
+  useEffect(() => {
+    if (val !== defaultValue) {
+      onChangeHandler?.(val)
+    }
+  }, [val, defaultValue, onChangeHandler])
+
+  const changeHandler = (evt: React.ChangeEvent<HTMLSelectElement>) => {
+    const evtVal = evt.currentTarget.value
+    setVal(evtVal)
+  }
+
+  return (
+    <s.SelectWrapper>
+      {title && <Label>{title}</Label>}
+      <s.Select {...rest} onChange={changeHandler} defaultValue={val}>
+        <s.Option key="empty" value="">
+          {placeholder || 'Please choose an option'}
         </s.Option>
-      ))}
-    </s.Select>
-  </s.SelectWrapper>
-)
+        {options.map((option, index) => (
+          <s.Option key={index} value={option.value}>
+            {option.label}
+          </s.Option>
+        ))}
+      </s.Select>
+    </s.SelectWrapper>
+  )
+}
