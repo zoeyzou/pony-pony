cfdd373022a97c4cadf93580ba6037429882d1a6
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-15 13:36:18 +0200

    Add global style and theme colors

--- src/sc/global-styles.ts
+++ src/sc/global-styles.ts
@@ -3,13 +3,19 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
    body {
       margin: 0;
-   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-      'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+         'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      background-image: url(${process.env.PUBLIC_URL}/img/dot-background.png);
+      background-size: cover;
    }
 
    code {
       font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+   }
+
+   *, * > * {
+      box-sizing: border-box;
    }
 `
--- src/sc/theme.ts
+++ src/sc/theme.ts
@@ -1,7 +1,12 @@
 export const theme = {
   colors: {
+    primary: '#AAABD3',
+    secondary: '#CBA6C3',
     dark: {
-      'shade-100': '#333',
+      'shade-100': '#353866',
+    },
+    light: {
+      'shade-100': '#F8FAFF',
     },
   },
 }
