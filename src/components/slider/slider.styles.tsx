177e052ef5496bf43fb353eb602ddd610dfcd9ad
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-15 22:54:20 +0200

    Add slider style

--- src/components/slider/slider.styles.tsx
+++ src/components/slider/slider.styles.tsx
@@ -5,6 +5,7 @@ export const SliderContainer = styled.div`
 `
 
 export const SliderWrapper = styled.div`
+  margin: 5px 0;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -12,8 +13,37 @@ export const SliderWrapper = styled.div`
 
 export const Slider = styled.input`
   flex: 1;
+  -webkit-appearance: none;
+  width: 100%;
+  height: 10px;
+  margin: 0 5px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
+  border-radius: 10px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    filter: brightness(90%);
+  }
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: ${({ theme }) => theme.colors.primary};
+    border-radius: 30px;
+    box-shadow: 0px 0px 3px darkgreen;
+    transition: all 0.5s ease;
+    position: relative;
+
+    &:hover,
+    &:active {
+      transform: scale(1.05);
+    }
+  }
 `
 
 export const Display = styled.span`
-  flex: 0;
+  width: 20px;
+  text-align: center;
 `
