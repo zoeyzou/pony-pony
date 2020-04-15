2a40871808b54a0f8bf17afe92d3f1ce3c70adc8
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-15 22:53:26 +0200

    Adjust maze style

--- src/components/maze/maze.styles.tsx
+++ src/components/maze/maze.styles.tsx
@@ -5,7 +5,7 @@ import { Props as CellProps } from './components/maze-cell'
 import { Direction } from 'types/directions'
 
 export const Maze = styled.div<Pick<Props, 'size'>>`
-  max-width: 55vw;
+  max-width: 80vh;
   display: grid;
   grid-template-columns: ${({ size }) => `repeat(${size![0]}, minmax(10px, 1fr))`};
   grid-auto-rows: 1fr;
@@ -28,36 +28,41 @@ export const CellWrapper = styled.div<CellProps>`
   position: relative;
   overflow: hidden;
   box-sizing: border-box;
-  margin: -0.2vw;
+  margin: -0.15vh;
   border-color: ${({ theme }) => theme.colors.dark['shade-100']};
 
   ${({ walls }) =>
     walls.includes(Direction.north) &&
     css`
-      border-top: 0.5vw solid;
+      border-top: 0.4vh solid;
     `}
 
   ${({ walls }) =>
     walls.includes(Direction.west) &&
     css`
-      border-left: 0.5vw solid;
+      border-left: 0.4vh solid;
     `}
 
   ${({ walls }) =>
     walls.includes(Direction.south) &&
     css`
-      border-bottom: 0.5vw solid;
+      border-bottom: 0.4vh solid;
     `}
 
   ${({ walls }) =>
     walls.includes(Direction.east) &&
     css`
-      border-right: 0.5vw solid;
+      border-right: 0.4vh solid;
     `}
 `
 
 export const Cell = styled.div`
   position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  padding: 0.4vh;
   width: 100%;
   height: 100%;
   display: flex;
